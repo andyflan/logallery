11ea398548ca66ee32f0853c1aa5c8d51d121d95
Andrew Flannery <andyflan@inheritweb.net>
2014-06-19 22:32:54 +0100

    Broken up the main function to make the code more readable

--- js/jquery.logallery.js
+++ js/jquery.logallery.js
@@ -33,11 +33,7 @@
 		
 		this.options = options;
 
-		this.init();
-
-		this.attach_events();
-
-		this.ready();
+		this.init().attach_events().ready();
 	}
 
 	Logallery.prototype.elements = null;
@@ -58,11 +54,7 @@
 			image_container:  	$(this.element).find(this.options.imageContainer),
 			thumbs_container: 	$(this.element).find('ul'),
 			caption_container: 	$(this.element).find(this.options.captionContainer) || $(this.element).nextAll(this.options.captionContainer)
-		}, 
-		w = $(this.elements.image_container).width();
-
-		this.elements.container.addClass('logallery');
-		this.elements.image_container.height(this.options.imageContainerInitialHeight + 'px');
+		};
 
 		//add the nav if we're using it
 		if (this.options.useNextPreviousNavigation) {
@@ -71,6 +63,12 @@
 
 			$(this.elements.image_container).append(this.elements.previous).append(this.elements.next);
 		}
+
+		//add the class and setup the initia height
+		this.elements.container.addClass('logallery');
+		this.elements.image_container.height(this.options.imageContainerInitialHeight + 'px');
+
+		return this;
 	}
 
 	Logallery.prototype.attach_events = function() {
@@ -79,66 +77,87 @@
 		//handle the nav links
 		$(this.elements.image_container).find('a.nav').on('click', function(element) {
 			if ($(this).hasClass('previous')) {
-				$(elements.thumbs_container).children().eq(activeIndex-1).find('a').click();
+				$(that.elements.thumbs_container).children().eq(that.activeIndex-1).find('a').trigger('click');
 			}
 
 			if ($(this).hasClass('next')) {
-				$(elements.thumbs_container).children().eq(activeIndex+1).find('a').click();
+				$(that.elements.thumbs_container).children().eq(that.activeIndex+1).find('a').trigger('click');
 			}
 				
 			return false;
 		});	
 
-		$(this.elements.thumbs_container).find('a').on('click', function(element) {
-			var img = new Image;
-				
-			$(that.elements.thumbs_container).find('a.active').removeClass('active')
-				
-			$(this).addClass('active');
-			
-			that.activeIndex = $(that.elements.thumbs_container).children().index($(this).parent());
-				
-			$(that.elements.image_container).addClass('loading');
-				
-			$(img).hide(); 
-				
-			$(img).load(function() {
-				$(that.elements.image_container).animate({ height: img.height });
-				$(that.elements.caption_container).append('<span>' + $(img).attr('alt') + '</span>');
-				//$(this).css('margin-left', (w - img.width) / 2 + 'px').appendTo(elements.image_container).removeClass('loading').fadeIn();
-				$(this).appendTo(that.elements.image_container).removeClass('loading').fadeIn();
-			}).attr({
-				src: $(this).attr('href'),
-				alt: $(this).attr('title'),
-				title: $(this).attr('title')
-			});
+		//handle the thumb clicks
+		$(this.elements.thumbs_container).find('a').on('click', function(event) {
+			event.preventDefault();
 
-			if (that.options.useNextPreviousNavigation) {
-				if (that.activeIndex == 0) {	
-					$(that.elements.previous).removeClass('active');
-				} else {
-					$(that.elements.previous).addClass('active');
-				}
-				
-				if (that.activeIndex == ($(that.elements.thumbs_container).children().length - 1)) {
-					$(that.elements.next).removeClass('active');
-				} else {
-					$(that.elements.next).addClass('active');
-				}
-			}
-							
-			$(that.elements.image_container).find('img').fadeOut(function() {
-				$(this).remove();
+			that.prepare_for_switch(this).load_image($(this).attr('href'), $(this).attr('title'), function() {
+				that.check_nav_controls();
 			});
-			
-			$(that.elements.caption_container).children().hide().remove();
-			
-			return false;
 		});
+
+		return this;
 	}
 
 	Logallery.prototype.ready = function() {
+		//we're ready, click the first thumn
 		$(this.elements.thumbs_container).find('li a:first').trigger('click');
+
+		return this;
+	}
+
+	Logallery.prototype.prepare_for_switch = function(thumb_a) {
+		//deactivate old thumb
+		$(this.elements.thumbs_container).find('a.active').removeClass('active')
+				
+		//activate the new one
+		$(thumb_a).addClass('active');
+			
+		//set the active index
+		this.activeIndex = $(this.elements.thumbs_container).children().index($(thumb_a).parent());
+			
+		//remove the caption
+		$(this.elements.caption_container).children().hide().remove();
+
+		//fade any existing image out then remove it
+		$(this.elements.image_container).find('img').fadeOut(function() {
+			$(thisx).remove();
+		});
+
+		return this;
+	}
+
+	Logallery.prototype.load_image = function(url, alt, callback) {
+		var that = this, img = new Image;
+
+		$(that.elements.image_container).addClass('loading');
+
+		$(img).hide().load(function() {
+			$(that.elements.image_container).animate({ height: img.height });
+			$(that.elements.caption_container).append('<span>' + $(img).attr('alt') + '</span>');
+			$(this).appendTo(that.elements.image_container).removeClass('loading').fadeIn();
+
+			callback();
+		}).attr({
+			src: url,
+			alt: alt
+		});
+	}
+
+	Logallery.prototype.check_nav_controls = function() {
+		if (this.options.useNextPreviousNavigation) {
+			if (this.activeIndex == 0) {	
+				$(this.elements.previous).removeClass('active');
+			} else {
+				$(this.elements.previous).addClass('active');
+			}
+			
+			if (this.activeIndex == ($(this.elements.thumbs_container).children().length - 1)) {
+				$(this.elements.next).removeClass('active');
+			} else {
+				$(this.elements.next).addClass('active');
+			}
+		}
 	}
 
 	//extend jquery
